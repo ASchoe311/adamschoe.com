b6c7512c678086883da5b70832627dd9707d15a6
Adam Schoenfeld <aschoe@umich.edu>
2022-03-15 20:02:58 -0400

    Fixes

--- bin/allunix.sh
+++ bin/allunix.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for file in $(find . -name '*.sh')
+for file in $(find bin -name '*.sh')
+do
+  $(dos2unix "$file")
+done
+for file in $(find .platform -name '*.sh')
+do
+  $(dos2unix "$file")
+done
+for file in $(find .ebextensions -name '*.config')
 do
   $(dos2unix "$file")
 done
